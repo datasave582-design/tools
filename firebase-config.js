2d6b6edb6f7468f437d29cb6f1b1568fdd79025c
datasave582-design <datasave582@gmail.com>
2026-08-09 18:52:22 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,12 @@
  */
 
 window.CSC_FIREBASE_CONFIG = {
-    apiKey: "AIzaSyA5LBLlGP83ol2nqWFrPmDS3zYMyA5c28",
-    authDomain: "cricket-c3052.firebaseapp.com",
-    databaseURL: "https://cricket-c3052-default-rtdb.firebaseio.com",
-    projectId: "cricket-c3052",
-    storageBucket: "cricket-c3052.firebasestorage.app",
-    messagingSenderId: "980803711949",
-    appId: "1:980803711949:web:8151d0a9903d8de5afca84",
-    measurementId: "G-M92NCSR69W"
+    apiKey: "AIzaSyA5LBLlGP83ol2nQwFrPmfDS3zYMyA5c28",
+  authDomain: "cricket-c3052.firebaseapp.com",
+  databaseURL: "https://cricket-c3052-default-rtdb.firebaseio.com",
+  projectId: "cricket-c3052",
+  storageBucket: "cricket-c3052.firebasestorage.app",
+  messagingSenderId: "980803711949",
+  appId: "1:980803711949:web:8151d0a9903d8de5afca84",
+  measurementId: "G-M92NCSR69W"
 };
