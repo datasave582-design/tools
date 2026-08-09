6e5f1e16abc25a3784ea672ddbead00288f14051
datasave582-design <datasave582@gmail.com>
2026-08-09 17:59:06 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA5LBLlGP83ol2nqWFrPmDS3zYMyA5c28",
+    authDomain: "cricket-c3052.firebaseapp.com",
+    databaseURL: "https://cricket-c3052-default-rtdb.firebaseio.com",
+    projectId: "cricket-c3052",
+    storageBucket: "cricket-c3052.firebasestorage.app",
+    messagingSenderId: "980803711949",
+    appId: "1:980803711949:web:8151d0a9903d8de5afca84",
+    measurementId: "G-M92NCSR69W"
+};
